bbda195ab923368c0c6ba002f77d6b98152ff178
Oleg Stukalov <oleg13stukalov@gmail.com>
2025-06-09 15:52:00 +0200

    switch to JSON logs, solving JSON problem with 'default' and timedelta

--- logs/startup.py
+++ logs/startup.py
@@ -7,10 +7,20 @@ from structlog.processors import JSONRenderer
 
 from logs.config import Config, LogsRenderer
 
+import datetime
+
 
 class AsyncBindableLogger(structlog.types.BindableLogger, structlog.stdlib.AsyncBoundLogger):  # type: ignore
     """Type fix for AsyncBoundLogger."""
 
+##### solving problem with 'default' and not serializeable timedelta
+def orjson_dumps(obj, *, default=None):
+    def fallback(x):
+        if isinstance(x, (datetime.datetime, datetime.date, datetime.timedelta)):
+            return str(x)
+        raise TypeError
+
+    return orjson.dumps(obj, default=fallback).decode()
 
 def startup(config: Config) -> None:
     pre_chain = [
@@ -31,10 +41,13 @@ def startup(config: Config) -> None:
     if config.renderer == LogsRenderer.text:
         renderer = structlog.dev.ConsoleRenderer(colors=True)
     elif config.renderer == LogsRenderer.json:
-        renderer = JSONRenderer(
-            serializer=lambda data: orjson.dumps(data).decode()
-            # serializer=json.dumps
-        )
+        # renderer = JSONRenderer(
+        #     serializer=lambda data: orjson.dumps(data).decode()
+        #     # serializer=json.dumps
+        #
+        # )
+        ##### solving problem with 'default' and not serializeable timedelta
+        renderer = JSONRenderer(serializer=orjson_dumps)
     else:
         raise ValueError('Logging: Unknown renderer set')
 
@@ -57,3 +70,8 @@ def startup(config: Config) -> None:
         wrapper_class=AsyncBindableLogger,
         cache_logger_on_first_use=True,
     )
+
+    #####
+    # print(f"***Config renderer: {config.renderer} (type: {type(config.renderer)})")
+    # print(f"***LogsRenderer.json: {LogsRenderer.json} (type: {type(LogsRenderer.json)})")
+    # print(f"***Equality test: {config.renderer == LogsRenderer.json}")
